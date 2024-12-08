2d0a9dd78e94f0d16a054cfd0587477afc4dab5a
shimoe2028 <160279414+shimoe2028@users.noreply.github.com>
2024-12-08 01:54:17 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cat for Fish
+
+A cat for fish.
+
+## Installation
+
+Use [Fisher](https://github.com/jorgebucaran/fisher):
+
+```fish
+fisher install shimoe2028/cat-for-fish
+```
---
+++ conf.d/cat.fish
@@ -0,0 +1,24 @@
+function cat-for-fish
+    if not status --is-interactive
+        return
+    end
+
+    set state_file ~/.cat_state
+
+    if test -f $state_file
+        set state (cat $state_file)
+
+        if test $state -eq 0
+            echo "Your cat brought you a new fish as a gift."
+            echo 1 > $state_file
+        end
+    else
+        echo "Your fish was eaten by your cat."
+        echo 0 > $state_file
+        exec bash
+    end
+end
+
+if status --is-interactive
+    cat-for-fish
+end
